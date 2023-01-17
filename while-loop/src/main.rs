b991b32020d7a68ead34a782887f4bf3114f443c
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-17 12:11:39 +0530

    Added program to demonstrate while loop in Rust by implementing Collatz conjecture

---
+++ while-loop/src/main.rs
@@ -0,0 +1,14 @@
+/* This Program demonstrates while loop by implementing Collatz conjecture */
+fn main() {
+    let mut x:i32 = 6;
+    print!("{x}");
+    while x != 1 {
+        if x % 2 == 0 {
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!(" -> {x}");
+    }
+    println!();
+}
