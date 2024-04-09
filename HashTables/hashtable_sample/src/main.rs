27fc33009637f1de37bb0118d8f8281ded41325f
Ramesh Vyas <rameshovyas@gmail.com>
2024-04-09 10:31:00 +0530

    sample code to demonstrate hashmap

---
+++ HashTables/hashtable_sample/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+type Table = HashMap<String, Vec<String>>;
+
+fn show(table: Table) {
+    for (domain, languages) in table {
+        println!("Languages used for {} ", domain);
+        println!("--------------------------------");
+        for language in languages {
+            println!("  {}", language);
+        }
+        println!("--------------------------------");
+    }
+}
+fn main() {
+    let mut table = Table::new();
+    table.insert(
+        "Systems Programming".to_string(),
+        vec![
+            "Assembly Language".to_string(),
+            "C Language".to_string(),
+            "Rust Language".to_string(),
+        ],
+    );
+
+    table.insert(
+        "Web Development".to_string(),
+        vec![
+            "C#".to_string(),
+            "Rust Language".to_string(),
+            "Java".to_string(),
+            "JavaScript".to_string(),
+        ],
+    );
+
+    show(table);
+}
